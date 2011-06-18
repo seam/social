ef7546c65176fde90ff0508c99a3ccbfabfb9440
Antoine Sabot-Durand <antoine@sabot-durand.net>
2011-06-18 22:22:50 +0200

    RESOLVED - issue SEAMSOCIAL-16: Follow Seam 3 example naming scheme https://issues.jboss.org/browse/SEAMSOCIAL-16

---
+++ example/web-client/src/main/java/org/jboss/seam/social/examples/webclient/SocialServiceConverter.java
@@ -0,0 +1,70 @@
+/**
+ *
+ */
+package org.jboss.seam.social.examples.webclient;
+
+import java.util.Map;
+
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Instance;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import javax.inject.Inject;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Maps;
+import org.jboss.seam.social.oauth.OAuthService;
+
+/**
+ * @author antoine
+ */
+@FacesConverter(value = "socialConverter", forClass = OAuthService.class)
+public class SocialServiceConverter implements Converter {
+
+    @Inject
+    @Any
+    private Instance<OAuthService> serviceHandlerInstances;
+
+    private Map<String, OAuthService> services;
+
+    private Map<String, OAuthService> getServices() {
+        if (services == null) {
+            services = Maps.uniqueIndex(serviceHandlerInstances, new Function<OAuthService, String>() {
+
+                @Override
+                public String apply(OAuthService arg0) {
+
+                    return arg0.getType();
+                }
+            });
+        }
+        return services;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see javax.faces.convert.Converter#getAsObject(javax.faces.context.FacesContext, javax.faces.component.UIComponent,
+     * java.lang.String)
+     */
+    @Override
+    public Object getAsObject(FacesContext arg0, UIComponent arg1, String arg2) {
+        Object res = getServices().get(arg2);
+        return ((OAuthService) res);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see javax.faces.convert.Converter#getAsString(javax.faces.context.FacesContext, javax.faces.component.UIComponent,
+     * java.lang.Object)
+     */
+    @Override
+    public String getAsString(FacesContext arg0, UIComponent arg1, Object arg2) {
+        String res = ((OAuthService) arg2).getType();
+        return res;
+    }
+
+}
